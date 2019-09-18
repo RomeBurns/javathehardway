c6c5e4516557abdf8ddfd6b2ae8f911952fe830c
Julian Brochu <nonspamerjbb@gmail.com>
2019-09-18 13:19:58 +0000

    upload

---
+++ 1-10/creatingvars.java
@@ -0,0 +1,22 @@
+public class creatingvars {
+    public static void main( String[] args ) {
+        int x, y, age;
+        double seconds, e, checking;
+        String firstname, lastname, title;
+
+        x = 10;
+        y = 400;
+        age = 39;
+
+        seconds = 4.171;
+        e = 2.71828182845904523536;
+        checking = 1.89;
+
+        firstname = "graham";
+        lastname = "Mitchell";
+        title = "Mr.";
+
+        System.out.println("The variable x contains: " + x);
+        System.out.println("The value" + y "is stored in the vair");
+    }
+}
