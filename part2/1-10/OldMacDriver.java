033910a72b4841f10370495838b449e09357968d
Julian Brochu <nonspamerjbb@gmail.com>
2019-12-13 14:21:04 +0000

    arjuisg

---
+++ part2/1-10/OldMacDriver.java
@@ -0,0 +1,13 @@
+public class OldMacDriver {
+    public static void main( String[] args ) {
+        OldMacCow maudine = new OldMacCow();
+        OldMacCow pauline = new OldMacCow();
+        maudine.moo();
+        pauline.moo();
+        OldMacPig snowball = new OldMacPig();
+        snowball.oink();
+        snowball.oink();
+        OldMacDuck ferdinand = new OldMacDuck();
+        ferdinand.quack();
+    }
+}
---
+++ part2/1-10/OldMacPig.java
@@ -0,0 +1,5 @@
+public class OldMacPig {
+    public void oink() {
+        System.out.println("Pig says oink.");
+    }
+}
